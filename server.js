3c37974a89f5fb9c1c689fe44adee21c928653d9
Patrick <43623131+21pxle@users.noreply.github.com>
2023-08-07 10:47:42 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+let env = require("./env.json");
+let axios = require("axios");
+
+axios.get("https://api.github.com/search/repositories?q=language:python+stars:%3E50000&sort=stars&order=asc", {
+    headers: {
+      Authorization: 'Bearer ' + env["token"]
+    }
+   }).then(response => {
+    if (response.status >= 400) {
+        console.log("Bad request");
+    } else {
+        for (let repo of response.data.items.slice(0, 5)) {
+            console.log(repo["full_name"]);
+        }
+    }
+}).catch(error => {
+    console.log(error);
+})
